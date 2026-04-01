570c5e2adb547d580eddd8500ac31eeb32a7f64a
lia-lopes <ame.pistache@gmail.com>
2026-04-01 13:52:14 -0300

    task:vercel deploy setup

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,6 @@
 import db from "@astrojs/db";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
@@ -9,4 +10,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  output: "static",
+  adapter: vercel(),
 });
